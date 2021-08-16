d9eef57ffb2aeb6296cc64651641c1cc0d281f7f
roger chat <mattrogerpelosi@gmail.com>
2021-08-16 11:16:11 -0400

    commit tree da24cb857056bdd83dfa4ddaf0112a0186d9219c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('input');
+
+
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
+}
+
+
+function clickAlert() {
+    alert('I was clicked!');
 }

203e915f4582400721e82ecbf89c3fec024848ce
roger chat <mattrogerpelosi@gmail.com>
2021-08-16 11:16:48 -0400

    Assignment Recomplete

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
+}
+
+function clickAlert() {
+    alert('I was clicked!');
 }
